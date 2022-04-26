15ff32cb37ba000c051f0ef759def7dd18d17236
EC2 Default User <ec2-user@ip-172-31-6-233.ec2.internal>
2022-04-26 13:57:10 +0000

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "2najveclm5f54bfr4i5n1ieuif",     // CognitoClientID
+  "api_base_url": "https://rwncdc6kmd.execute-api.us-east-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-serverlesspractice.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1imtjhva0yf04.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
